df5fcf8441c12deeb84ae8a991e7f6e6d1a4e417
Anonymous <anonymous@example.com>
2022-01-13 22:08:10 +1030

    statistics

--- amm_generator.py
+++ amm_generator.py
@@ -3,7 +3,7 @@ from scipy.sparse import csr_matrix
 
 class AmmGenerator:
 
-    def greedy_combined_selection_itv(self, x_dataset_matrix: csr_matrix, s_matrix_shap: numpy.ndarray, feature_names: list, trigger_size=75):
+    def AMM_feature_selection(self, x_dataset_matrix: csr_matrix, s_matrix_shap: numpy.ndarray, feature_names: list, trigger_size=75):
 
         if type(x_dataset_matrix) is csr_matrix:
             x_local = x_dataset_matrix.toarray()
@@ -36,3 +36,53 @@ class AmmGenerator:
             output[feature] = value
 
         return output
+
+    def statistics_feature_selection(self, x, y, feature_names, select_num=300):
+        dimension = x.shape[1]
+        x_ben = []
+        x_mal = []
+        for index in len(y):
+            if y == 0:
+                x_ben.append(x[index])
+            else:
+                x_mal.append(x[index])
+        
+        x_benign = numpy.array(x_ben)
+        x_malicious = numpy.array(x_mal)
+        bens_sum = x_benign.sum(axis=0).tolist()[0]
+        mal_sum = x_malicious.sum(axis=0).tolist()[0]
+
+        mal_top = mal_sum.copy()
+        mal_top.sort(reverse=True)
+        mal_top_2 = [item for item in mal_top if item > 0]
+        border_mal_top = mal_top_2[int(len(mal_top_2) * 0.1)]
+        border_mal_bottom = mal_top_2[int(len(mal_top_2) * 0.9)]
+
+        ben_top = bens_sum.copy()
+        ben_top.sort(reverse=True)
+        ben_top_2 = [item for item in ben_top if item > 0]
+        border_ben_top = ben_top_2[int(len(ben_top_2) * 0.1)]
+        border_ben_bottom = ben_top_2[int(len(ben_top_2) * 0.9)]
+
+        mal_indexes = []
+        ben_indexes = []
+        for index in range(dimension):
+            ben_int = bens_sum[index]
+            mal_int = mal_sum[index]
+            if ben_int >= border_ben_top and mal_int <= border_mal_bottom:
+                ben_indexes.append(index)
+            elif mal_int >= border_mal_top and ben_int <= border_ben_bottom:
+                mal_indexes.append(index)
+
+        mal_top_map = {value: mal_sum[value] for value in mal_indexes}
+        mal_top_map = dict(sorted(mal_top_map.items(), key=lambda item: item[1], reverse=True))
+        mal_top_fts = [key for key in mal_top_map][:select_num]
+        
+        ben_top_map = {value: bens_sum[value] for value in ben_indexes}
+        ben_top_map = dict(sorted(ben_top_map.items(), key=lambda item: item[1], reverse=True))
+        ben_top_fts = [key for key in ben_top_map][:select_num]
+
+        mal_names = [feature_names[index] for index in mal_top_fts]
+        ben_names = [feature_names[index] for index in ben_top_fts]
+
+        return mal_names, ben_names
